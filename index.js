d4ac51155b597325ba920eb1e84f378cbae688a9
Abhinav Awana <abhinavawana2444@gmail.com>
2024-08-30 14:56:51 +0530

    completed nav bar and theme toggle

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// dark theme switch start
+let darktheme = localStorage.getItem("darktheme");
+const body = document.querySelector(".body");
+const themeToggle = document.querySelector("#theme-toggle");
 
+function enableDarktheme() {
+  body.classList.add("darktheme");
+  localStorage.setItem("darktheme", "active");
+}
+function disableDarktheme() {
+  body.classList.remove("darktheme");
+  localStorage.setItem("darktheme", null);
+}
+if (darktheme === "active") enableDarktheme();
+themeToggle.addEventListener("click", () => {
+  darktheme = localStorage.getItem("darktheme");
+  darktheme !== "active" ? enableDarktheme() : disableDarktheme();
+});
+// dark theme switch ends
